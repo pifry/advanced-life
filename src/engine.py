cfb0ff2ed1ede4b2f62ccf399987dff6af4edb81
Piotr Fryc <piotr.fryc@gmail.com>
2022-10-30 00:33:15 +0200

    Working multidimension mode

--- src/engine.py
+++ src/engine.py
@@ -1,4 +1,6 @@
+from turtle import pos
 import numpy as np
+from itertools import product
 
 # rng = np.random.default_rng(12345)
 # world = rng.choice([False, True], CANVAS_SIZE)
@@ -9,9 +11,48 @@ class Engine:
     def random(cls, size):
         return cls(size, random=True)
 
-    def __init__(self, size, random=False, default=False) -> None:
+    def __init__(self, size, random=False, default=0) -> None:
         if random:         
-            self.data = RNG.choice([False, True], size)
+            self.data = RNG.choice([0, 1], size)
         else:
             self.data = np.ndarray(size)
             self.data.fill(default)
+
+        self.epoch_no = 0
+
+        self.die_population = list(range(0,7)) + list(range(15,27)) 
+        self.born_population = [11,12]
+
+        # Classical Conway's Game of Life Rules
+        # self.die_population = list(range(0,2)) + list(range(4,9)) 
+        # self.born_population = [3]
+
+    def epoch(self):
+        new_data = np.ndarray(self.data.shape)
+
+        ranges = [range(dim) for dim in self.data.shape]
+
+        activity = 0
+        size = 0
+
+        for position in product(*ranges):
+            nbhd = self.data
+            for dim in range(self.data.ndim):
+                nbhd = nbhd.take(indices=range(position[dim]-1, position[dim]+1+1), axis=dim, mode='wrap')
+
+            population = np.sum(nbhd) - self.data[position]
+
+            new_data[position] = self.data[position]
+            if population in self.die_population:
+                new_data[position] = 0
+            if population in self.born_population:
+                new_data[position] = 1
+
+            if new_data[position] != self.data[position]:
+                activity = activity + 1
+            if new_data[position] == 1:
+                size = size + 1
+            
+        self.data = new_data
+        print(f'epoch {self.epoch_no}: activity: {activity}, size: {size}')
+        self.epoch_no = self.epoch_no + 1
--- src/main.py
+++ src/main.py
@@ -3,18 +3,21 @@ import random
 import string
 
 from engine import Engine
+import time
+import threading
 
 sg.theme('DarkAmber')
 sg.theme('SystemDefault')
 
 CANVAS_SIZE = (20, 20)
+WORLD_SIZE = (CANVAS_SIZE[0], CANVAS_SIZE[1], 20)
 BOX_SIZE = 25
 
 layout = [
     [sg.Text("Multidimensional Conway's Game of Life"), sg.Text('', key='-OUTPUT-')],
     [sg.Graph((CANVAS_SIZE[0]*BOX_SIZE, CANVAS_SIZE[1]*BOX_SIZE), (0, CANVAS_SIZE[1]*BOX_SIZE), (CANVAS_SIZE[0]*BOX_SIZE, 0), key='-GRAPH-',
               change_submits=True, drag_submits=False)],
-    [sg.Button('Start'), sg.Button("Random"), sg.Button('Exit')]
+    [sg.Button('Start'), sg.Button("Random"), sg.Button('Stop'), sg.Button('Exit')]
 ]
 
 window = sg.Window('Window Title', layout, finalize=True)
@@ -32,10 +35,18 @@ def update_graph(g, data):
     width, height = CANVAS_SIZE
     for x in range(width):
         for y in range(height):
-            if data[x,y]:
+            if data[x,y,0] == 1:
                 g.draw_rectangle((x * BOX_SIZE, y * BOX_SIZE), (x * BOX_SIZE + BOX_SIZE, y * BOX_SIZE + BOX_SIZE), line_color='black', fill_color='black')
             else:
                 g.draw_rectangle((x * BOX_SIZE, y * BOX_SIZE), (x * BOX_SIZE + BOX_SIZE, y * BOX_SIZE + BOX_SIZE), line_color='black', fill_color='white')
+
+def engine_thread_func(window, world, event):
+    while not event.is_set():
+        time.sleep(0.5)
+        world.epoch()
+        update_graph(window['-GRAPH-'], world.data)
+
+stop_event = None
 
 while True:             # Event Loop
     event, values = window.read()
@@ -45,10 +56,15 @@ while True:             # Event Loop
     mouse = values['-GRAPH-']
 
     if event == 'Random':
-        world = Engine.random(CANVAS_SIZE)
+        world = Engine.random(WORLD_SIZE)
         update_graph(g, world.data)
-
-    if event == '-GRAPH-':
+    elif event == 'Start':
+        stop_event = threading.Event()
+        threading.Thread(target=engine_thread_func, args=(window, world, stop_event), daemon=True,).start()
+    elif event == 'Stop':
+        if stop_event:
+            stop_event.set()
+    elif event == '-GRAPH-':
         if mouse == (None, None):
             continue
         update_graph(g, world.data)
